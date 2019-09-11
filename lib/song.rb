b413037b8b24350adfda539108af21e5320b8922
Laura Berge <lauraeberge@gmail.com>
2019-09-11 00:27:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,8 +49,8 @@ class Song
     song_artist = filename.split(".")[0]
     array = song_artist.split("-")
     self.new_by_name(array[1])
-    @name = array[1]
-    @artist_name = array[0]
+    self.name = array[1]
+    self.artist_name = array[0]
   end
   
   def self.create_from_filename(filename)
